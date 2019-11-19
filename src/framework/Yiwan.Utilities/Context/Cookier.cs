b77502e1ed1af5a5c6b8425c5f1d772526e312c5
万里企鹅 <lileyzhao@gmail.com>
2019-11-19 19:04:55 +0800

    优化cookie&session操作类，新增泛型支持

---
+++ src/framework/Yiwan.Utilities/Context/Cookier.cs
@@ -0,0 +1,175 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Globalization;
+using System.Web;
+
+namespace Yiwan.Utilities
+{
+    /// <summary>
+    /// Cookie操作类(Cookie的增删查改) By @yiwanlee liley@foxmail.com 秋秋:897250000
+    /// 1、Get(string key)获取Cookie对象
+    /// 2、Set(string key, string value)设置Cookie
+    /// 3、Remove(string key)删除Cookie
+    /// 4、Clear()清空Cookie
+    /// </summary>
+    public static class Cookier
+    {
+        /// <summary>
+        /// 删除一个指定的Cookie
+        /// </summary>
+        /// <param name="key">Cookie的键</param>
+        public static void Remove(string key, HttpContext context = null)
+        {
+            context = context ?? HttpContext.Current;
+            if (context == null) return;
+
+            HttpCookie cookie = context.Request.Cookies[key];
+            if (cookie != null)
+            {
+                CultureInfo cti = CultureInfo.CurrentCulture;
+                cookie.Expires = DateTime.Parse("1900-01-01", cti.DateTimeFormat);
+                context.Response.Cookies.Add(cookie);
+            }
+        }
+
+        /// <summary>
+        /// 移除所有的Cookie 等价于 Clear
+        /// </summary>
+        public static void RemoveAll(HttpContext context = null)
+        {
+            context = context ?? HttpContext.Current;
+            if (context == null) return;
+
+            foreach (string name in context.Request.Cookies.AllKeys)
+            {
+                Remove(name, context);
+            }
+        }
+
+        /// <summary>
+        /// 清除所有的Cookie 等价于 RemoveAll
+        /// </summary>
+        public static void Clear(HttpContext context = null)
+        {
+            RemoveAll(context);
+        }
+
+        /// <summary>
+        /// 获取指定Cookie
+        /// </summary>
+        /// <param name="key">Cookie的键</param>
+        public static HttpCookie Get(string key, HttpContext context = null)
+        {
+            context = context ?? HttpContext.Current;
+            if (context == null) return null;
+
+            HttpCookie cookie = context.Request.Cookies[key];
+            return cookie;
+        }
+
+        /// <summary>
+        /// 获取指定Cookie的值
+        /// </summary>
+        /// <param name="key">Cookie的键</param>
+        public static string GetValue(string key, HttpContext context = null)
+        {
+            context = context ?? HttpContext.Current;
+            if (context == null) return string.Empty;
+
+            HttpCookie cookie = Get(key, context);
+            if (cookie != null) return cookie.Value;
+            else return string.Empty;
+        }
+
+        /// <summary>
+        /// 获取指定Cookie的值
+        /// </summary>
+        /// <param name="key">Cookie的键</param>
+        public static T GetValue<T>(string key, HttpContext context = null)
+        {
+            context = context ?? HttpContext.Current;
+            if (context == null) return default;
+
+            HttpCookie cookie = Get(key, context);
+            if (cookie != null) return Convert2Object<T>(cookie.Value);
+            else return default;
+        }
+
+        /// <summary>
+        /// 添加一个Cookie
+        /// </summary>
+        /// <param name="key">Cookie的键</param>
+        /// <param name="value">Cookie的值</param>
+        /// <param name="expires">过期时间 DateTime</param>
+        public static void Set(string key, string value, DateTime? expires, HttpContext context = null)
+        {
+            context = context ?? HttpContext.Current;
+            if (context == null) return;
+
+            HttpCookie cookie = new HttpCookie(key)
+            {
+                Value = value
+            };
+            if (expires != null) cookie.Expires = (DateTime)expires;
+
+            context.Response.Cookies.Add(cookie);
+        }
+
+        /// <summary>
+        /// 添加一个Cookie
+        /// </summary>
+        /// <typeparam name="T">值的类型</typeparam>
+        /// <param name="key">Cookie的名称</param>
+        /// <param name="value">Cookie的值</param>
+        /// <param name="expires">过期时间 DateTime</param>
+        public static void Set<T>(string key, T value, DateTime? expires, HttpContext context = null)
+        {
+            Set(key, Convert2Json(value), expires, context);
+        }
+
+        /// <summary>
+        /// 添加一个Cookie（24小时过期）
+        /// </summary>
+        /// <param name="name">Cookie的名称</param>
+        /// <param name="cookievalue">Cookie的键</param>
+        public static void Set(string key, string value, HttpContext context = null)
+        {
+            Set(key, value, null, context);
+        }
+
+        /// <summary>
+        /// 添加一个Cookie（不设置过期时间，随浏览器生命周期）
+        /// </summary>
+        /// <typeparam name="T">值的类型</typeparam>
+        /// <param name="key">Cookie的名称</param>
+        /// <param name="value">Cookie的键</param>
+        public static void Set<T>(string key, T value, HttpContext context = null)
+        {
+            Set(key, Convert2Json(value), null, context);
+        }
+
+        #region 内部私有函数
+        /// <summary>
+        /// 序列化对象为Json字符串
+        /// </summary>
+        private static string Convert2Json<T>(T value)
+        {
+            string result = value is string ? value.ToString() : JsonConvert.SerializeObject(value);
+            return result;
+        }
+
+        /// <summary>
+        /// 反序列化Json字符串为Object对象
+        /// </summary>
+        private static T Convert2Object<T>(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value)) return default;
+            else if (typeof(T).Name.Equals(typeof(string).Name, StringComparison.CurrentCulture))
+            {
+                return JsonConvert.DeserializeObject<T>($"'{value}'");
+            }
+            return JsonConvert.DeserializeObject<T>(value);
+        }
+        #endregion 内部私有函数
+    }
+}
---
+++ src/framework/Yiwan.Utilities/Context/Sessioner.cs
@@ -0,0 +1,171 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Yiwan.Utilities
+{
+    /// <summary>
+    /// Session操作类(Session的增删查改) By @yiwanlee liley@foxmail.com 秋秋:897250000
+    /// 1、Get(string key)获取Session对象
+    /// 2、Set(string key, string value)设置Session
+    /// 3、Remove(string key)删除Session
+    /// 4、Clear()清空Session
+    /// </summary>
+    public static class Sessioner
+    {
+        /// <summary>
+        /// 删除一个指定的Session
+        /// </summary>
+        /// <param name="key">Session的名称</param>
+        public static void Remove(string name, HttpContext context = null)
+        {
+            context = context ?? HttpContext.Current;
+            if (context == null) return;
+
+            context.Session.Remove(name);
+        }
+
+        /// <summary>
+        /// 移除所有的Session 等价于 Clear
+        /// </summary>
+        public static void RemoveAll(HttpContext context = null)
+        {
+            context = context ?? HttpContext.Current;
+            if (context == null) return;
+
+            context.Session.Clear();
+        }
+
+        /// <summary>
+        /// 清空所有的Session
+        /// </summary>
+        public static void Clear(HttpContext context = null)
+        {
+            RemoveAll(context);
+        }
+
+        /// <summary>
+        /// 根据Session的键获取Session对象
+        /// </summary>
+        /// <param name="key">Session的键</param>
+        public static string Get(string key, HttpContext context = null)
+        {
+            context = context ?? HttpContext.Current;
+            if (context == null) return string.Empty;
+
+            if (context.Session[key] != null) return context.Session[key].ToString();
+            else return string.Empty;
+        }
+
+        /// <summary>
+        /// 根据Session的键获取Session对象
+        /// </summary>
+        /// <param name="key">Session的键</param>
+        public static T Get<T>(string key, HttpContext context = null)
+        {
+            context = context ?? HttpContext.Current;
+            if (context == null) return default;
+
+            if (context.Session[key] != null) return Convert2Object<T>(context.Session[key].ToString());
+            else return default;
+        }
+
+        /// <summary>
+        /// 设置一个Session
+        /// </summary>
+        /// <param name="key">Session的键</param>
+        /// <param name="value">Session的值</param>
+        public static void Set(string key, string value, HttpContext context = null)
+        {
+            context = context ?? HttpContext.Current;
+            if (context == null) return;
+
+            context.Session.Remove(key);
+            context.Session.Add(key, value);
+        }
+
+        /// <summary>
+        /// 设置一个Session
+        /// </summary>
+        /// <param name="key">Session的键</param>
+        /// <param name="value">Session的值</param>
+        public static void Set<T>(string key, T value, HttpContext context = null)
+        {
+            context = context ?? HttpContext.Current;
+            if (context == null) return;
+
+            context.Session.Remove(key);
+            context.Session.Add(key, Convert2Json(value));
+        }
+
+        /// <summary>
+        /// 设置一个Session
+        /// </summary>
+        /// <param name="key">Session的键</param>
+        /// <param name="value">Session的值</param>
+        /// <param name="timeout">在??分钟后失效</param>
+        public static void Set(string key, string value, int timeout, HttpContext context = null)
+        {
+            context = context ?? HttpContext.Current;
+            if (context == null) return;
+
+            Set(key, value, context);
+            context.Session.Timeout = timeout;
+        }
+
+        /// <summary>
+        /// 设置一个Session
+        /// </summary>
+        /// <param name="key">Session的键</param>
+        /// <param name="value">Session的值</param>
+        /// <param name="timeout">在??分钟后失效</param>
+        public static void Set<T>(string key, T value, int timeout, HttpContext context = null)
+        {
+            context = context ?? HttpContext.Current;
+            if (context == null) return;
+
+            Set(key, value, context);
+            context.Session.Timeout = timeout;
+        }
+
+        /// <summary>
+        /// 设置Session过期时间
+        /// </summary>
+        /// <param name="timeout">在??分钟后失效</param>
+        public static void SetTimeout(int timeout, HttpContext context = null)
+        {
+            context = context ?? HttpContext.Current;
+            if (context == null) return;
+
+            context.Session.Timeout = timeout;
+        }
+
+        #region 内部私有函数
+        /// <summary>
+        /// 序列化对象为Json字符串
+        /// </summary>
+        private static string Convert2Json<T>(T value)
+        {
+            string result = value is string ? value.ToString() : JsonConvert.SerializeObject(value);
+            return result;
+        }
+
+        /// <summary>
+        /// 反序列化Json字符串为Object对象
+        /// </summary>
+        private static T Convert2Object<T>(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value)) return default;
+            else if (typeof(T).Name.Equals(typeof(string).Name, StringComparison.CurrentCulture))
+            {
+                return JsonConvert.DeserializeObject<T>($"'{value}'");
+            }
+            return JsonConvert.DeserializeObject<T>(value);
+        }
+        #endregion 内部私有函数
+    }
+}
