b4c0b3c455288fcdea9e80c2f96334a685a3e383
万里QEPC <lileyzhao@gmail.com>
2019-11-23 13:40:04 +0800

    修改程序集信息

--- src/framework/Yiwan.Helpers/Properties/AssemblyInfo.cs
+++ src/framework/Yiwan.Helpers/Properties/AssemblyInfo.cs
@@ -4,10 +4,10 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Yiwan.Helpers")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("C#.NET通用帮助类")]
+[assembly: AssemblyDescription("在C#.NET开发过程中常用的帮助类")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Yiwanlee")]
 [assembly: AssemblyProduct("Yiwan.Helpers")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.19.0")]
+[assembly: AssemblyFileVersion("0.1.19.11231")]
