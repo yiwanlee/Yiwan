c6ba3ce6ffa05bb5dfa92bba7633fb0aced5bb99
一万里XPS <lileyzhao@gmail.com>
2019-11-26 22:10:30 +0800

    修改logs为logs2

---
+++ YiwanOLD/Yiwan.Utilities/Logs2/ElmahLog.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Yiwan.Utilities
+{
+    public static class ElmahLog
+    {
+        public static string Log(string message)
+        {
+            var Context = System.Web.HttpContext.Current;
+            try { var req = System.Web.HttpContext.Current.Request; }
+            catch (System.Web.HttpException) { Context = null; }
+
+            return Elmah.ErrorLog.GetDefault(Context).Log(new Elmah.Error(new Exception("#日志#:" + message)));
+        }
+        public static string Log(string message, Exception innerException)
+        {
+            var Context = System.Web.HttpContext.Current;
+            try { var req = System.Web.HttpContext.Current.Request; }
+            catch (System.Web.HttpException) { Context = null; }
+
+            return Elmah.ErrorLog.GetDefault(Context).Log(new Elmah.Error(new Exception("#日志#:" + message, innerException)));
+        }
+
+        public static string Error(string message)
+        {
+            var Context = System.Web.HttpContext.Current;
+            try { var req = System.Web.HttpContext.Current.Request; }
+            catch (System.Web.HttpException) { Context = null; }
+
+            return Elmah.ErrorLog.GetDefault(Context).Log(new Elmah.Error(new Exception("#错误#:" + message)));
+        }
+        public static string Error(string message, Exception innerException)
+        {
+            var Context = System.Web.HttpContext.Current;
+            try { var req = System.Web.HttpContext.Current.Request; }
+            catch (System.Web.HttpException) { Context = null; }
+
+            return Elmah.ErrorLog.GetDefault(Context).Log(new Elmah.Error(new Exception("#错误#:" + message, innerException)));
+        }
+        public static string Error(Exception exception)
+        {
+            var Context = System.Web.HttpContext.Current;
+            try { var req = System.Web.HttpContext.Current.Request; }
+            catch (System.Web.HttpException) { Context = null; }
+
+            return Elmah.ErrorLog.GetDefault(Context).Log(new Elmah.Error(exception));
+        }
+    }
+}
---
+++ YiwanOLD/Yiwan.Utilities/Logs2/NLogger.cs
@@ -0,0 +1,25 @@
+﻿using NLog;
+
+namespace Yiwan.Utilities
+{
+    public class NLogger
+    {
+        private static readonly object mutex = new object();
+        private static Logger _logger = LogManager.GetCurrentClassLogger();
+
+        public static Logger Current
+        {
+            get
+            {
+                if (_logger == null)
+                {
+                    lock (mutex)
+                    {
+                        if (_logger == null) _logger = LogManager.GetCurrentClassLogger();
+                    }
+                }
+                return _logger;
+            }
+        }
+    }
+}
