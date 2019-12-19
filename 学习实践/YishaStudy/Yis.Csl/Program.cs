7ce7760272689c51b734dea6ad17b0d788e86425
万里QEPC <lileyzhao@gmail.com>
2019-12-19 18:41:54 +0800

    s191218

---
+++ 学习实践/YishaStudy/Yis.Csl/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Yis.Csl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Prov prov = null;
+            ProvInner pi = prov?.PI;
+            Console.WriteLine("Prov.Name:" + pi?.name);
+
+            string dir = AppContext.BaseDirectory;
+            
+
+            Console.WriteLine(dir);
+        }
+
+        class Prov
+        {
+            public ProvInner PI { get; set; }
+        }
+
+        class ProvInner
+        {
+            public string name { get; set; }
+
+            public override string ToString()
+            {
+                return name;
+            }
+        }
+    }
+}
---
+++ 学习实践/YishaStudy/Yis.Csl/QebbDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yis.Csl
+{
+    public class QebbDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=blogging.db");
+        }
+    }
+}
