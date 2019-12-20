2961cdea2cc3c620d0b7edcbad314c1e165cad6d
万里QEPC <lileyzhao@gmail.com>
2019-12-20 18:27:51 +0800

    s191220

--- 学习实践/YishaStudy/Yis.Csl/Program.cs
+++ 学习实践/YishaStudy/Yis.Csl/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Yis.Csl
 {
@@ -13,9 +14,44 @@ namespace Yis.Csl
             Console.WriteLine("Prov.Name:" + pi?.name);
 
             string dir = AppContext.BaseDirectory;
-            
 
-            Console.WriteLine(dir);
+            if (true)
+            {
+                System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+                stopwatch.Start();
+
+                List<long> ids = new List<long>();
+                for (int i = 0; i < 10000000; i++) ids.Add(IdGeneratorHelper.Instance.GetId());
+
+                stopwatch.Stop();
+                Console.WriteLine("消耗：" + stopwatch.ElapsedMilliseconds);
+            }
+
+            if (true)
+            {
+                System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+                stopwatch.Start();
+
+                List<Guid> ids = new List<Guid>();
+                for (int i = 0; i < 10000000; i++) ids.Add(Guid.NewGuid());
+
+                stopwatch.Stop();
+                Console.WriteLine("消耗：" + stopwatch.ElapsedMilliseconds);
+            }
+
+            Console.WriteLine(2 & 2);
+
+            // 总位 32 减去 符号位 实际 31
+            // 机器位 3 最大8 剩余 28
+            // 序号位 6 最大64 剩余 22
+        }
+
+        static string Bu0(string s, int l = 64)
+        {
+            if (s.Length >= l) return s;
+            string c0 = "";
+            for (int i = 0; i < l - s.Length; i++) c0 += "0";
+            return c0 + s;
         }
 
         class Prov
