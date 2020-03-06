de4065704083ddc38e3804d332a9a7928b0bf598
万里QEPC <lileyzhao@gmail.com>
2020-03-06 18:10:49 +0800

    s200306

--- 学习实践/前端学习/ts/ts01/hello.js
+++ 学习实践/前端学习/ts/ts01/hello.js
@@ -1,8 +1,48 @@
-function buildName(firstName, lastName) {
-    if (lastName === void 0) { lastName = 'Tom'; }
-    return firstName + ' ' + lastName;
-}
-var tomcat = buildName('Tom', 'Cat');
-var cat = buildName('Cat', undefined);
-console.log(tomcat);
-console.log(cat);
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Animal = /** @class */ (function () {
+    function Animal(name) {
+        this.name = name;
+    }
+    Animal.prototype.SayHi = function () {
+        return "Hi " + this.name;
+    };
+    return Animal;
+}());
+var Cat = /** @class */ (function (_super) {
+    __extends(Cat, _super);
+    function Cat() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Cat.prototype.SayHi = function () {
+        return _super.prototype.SayHi.call(this) + " #Cat#";
+    };
+    return Cat;
+}(Animal));
+var Dog = /** @class */ (function (_super) {
+    __extends(Dog, _super);
+    function Dog() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Dog.prototype.SayHi = function () {
+        return _super.prototype.SayHi.call(this) + " #Dog#";
+    };
+    return Dog;
+}(Animal));
+var a = new Animal("薇薇");
+console.log(a.SayHi());
+var cat = new Cat("薇薇");
+console.log(cat.SayHi());
+var dog = new Dog("薇薇");
+console.log(dog.SayHi());
--- 学习实践/前端学习/ts/ts01/hello.ts
+++ 学习实践/前端学习/ts/ts01/hello.ts
@@ -1,9 +1,27 @@
-function buildName(firstName: string, lastName: string = 'Tom') {
-    return firstName + ' ' + lastName;
+class Animal {
+    public name: string;
+    constructor(name: string) {
+        this.name = name;
+    }
+    SayHi() {
+        return `Hi ${this.name}`
+    }
 }
-let tomcat = buildName('Tom', 'Cat');
-let cat = buildName('Cat', undefined);
 
-console.log(tomcat);
+class Cat extends Animal {
+    SayHi() {
+        return `${super.SayHi()} #Cat#`
+    }
+}
+class Dog extends Animal {
+    SayHi() {
+        return `${super.SayHi()} #Dog#`
+    }
+}
 
-console.log(cat);
+let a: Animal = new Animal("薇薇");
+console.log(a.SayHi())
+let cat = new Cat("薇薇");
+console.log(cat.SayHi())
+let dog = new Dog("薇薇");
+console.log(dog.SayHi())
