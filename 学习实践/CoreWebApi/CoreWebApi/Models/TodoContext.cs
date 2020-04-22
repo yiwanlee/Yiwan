a07cc17fdbb911e6885750a13e3456e0f880d23f
万里QEPC <lileyzhao@gmail.com>
2020-04-22 18:02:05 +0800

    添加CoreWebApi学习项目

---
+++ 学习实践/CoreWebApi/CoreWebApi/Models/TodoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoreWebApi
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItem { get; set; }
+    }
+}
